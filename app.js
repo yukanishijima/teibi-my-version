aa005c7d605c89370b47adb3ef7b981fa5366af4
cjstanzhu <cjstanzhu@gmail.com>
2019-10-26 11:53:19 -0400

    update app.js with passport

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static("client/build"));
 
 app.use('/', indexRouter);
