ef32deca6e79e347637e4e8a666b7918d5b4c007
cjstanzhu <cjstanzhu@gmail.com>
2019-10-26 13:22:57 -0400

    update signup page

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -1,6 +1,38 @@
 import React, { Component } from 'react';
+import API from "../utils/API";
 
 class SignUp extends Component {
+    state = {
+        username: "",
+        email: "",
+        password: ""
+    };
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({[name]: value});
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+
+        var userData = {
+            username: this.state.username,
+            email: this.state.email,
+            password: this.state.password
+        };
+
+        API.saveUser({userData})
+            .then(res => {
+                console.log("user saved");
+
+            })
+            .catch(err => {
+                console.log(err)
+
+            });
+
+    };
 
     render() {
         return (
