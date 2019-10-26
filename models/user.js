1c9119b9a65df1ef070d7f8baf819de4ef976a57
cjstanzhu <cjstanzhu@gmail.com>
2019-10-26 13:38:23 -0400

    insert bcryptjs code into user model

--- models/user.js
+++ models/user.js
@@ -1,10 +1,25 @@
+var bcrypt = require("bcryptjs");
+
 const mongoose = require("mongoose");
+
 const Schema = mongoose.Schema;
+
 const userSchema = new Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
   date: { type: Date, default: Date.now }
 });
+
 const User = mongoose.model("User", userSchema);
+
+User.prototype.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+User.addHook("beforeCreate", function(user) {
+  user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+});
+
 module.exports = User;
+
