e135bfc3325c912e0cf522b7ebf46ac1f523c223
Neha Gupta <er.nehaa.gupta@gmail.com>
2019-10-26 12:40:45 -0400

    updating userPage with ref

--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true },
+  username: { type: String, unique: true, required: true },
+  email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   date: { type: Date, default: Date.now }
 });
