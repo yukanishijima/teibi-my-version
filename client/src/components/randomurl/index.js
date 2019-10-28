fccf49ed517db91d11dd1bb5219d0f4a06e44b92
Héctor Morales <hector@morales.is>
2019-10-28 16:50:20 -0400

    increase id length

--- client/src/components/randomurl/index.js
+++ client/src/components/randomurl/index.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 function randomID(){
     let idText = ""
-    for (let i = 0; i < 6; i++) {
-        if(i === 3){
+    for (let i = 0; i < 8; i++) {
+        if(i === 4){
             idText += "-"
         }
         idText += String.fromCharCode(65 + Math.floor(Math.random() * 26))
