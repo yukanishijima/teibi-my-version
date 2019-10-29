f140b8b8cafe5164f21a8a1cad6d52ca5d715a74
Héctor Morales <hector@morales.is>
2019-10-29 11:09:53 -0400

    add notes close #10

--- client/src/components/randomurl/index.js
+++ client/src/components/randomurl/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
+// Generate a random ID XXXX-XXXX
+// Currently set to letters only in uppercase
 function randomID(){
     let idText = ""
     for (let i = 0; i < 8; i++) {
@@ -12,7 +14,7 @@ function randomID(){
     return idText
 }
 
-
+// Returns the Link to a random URL
 function RandomUrl() {
     return (
         <>
