1aeed04918dcd2de503aa0b6639543e25267bfc0
Héctor Morales <hector@morales.is>
2019-10-29 14:04:18 -0400

    autofix, eslint

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,4 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 // const axios = require('axios');
 
@@ -7,16 +7,20 @@ require("dotenv").config();
 
 ("use strict");
 
-const yelp = require('yelp-fusion');
+const yelp = require("yelp-fusion");
 const client = yelp.client(process.env.apikey);
 
-const dbController = require('../controllers/dbControllers');
+// const dbController = require("../controllers/dbControllers");
 
 // use mongoose
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 //Set up default mongoose connection
 var mongoDB = `mongodb://${process.env.user}:${process.env.password}@${process.env.host}/${process.env.db}`;
-mongoose.connect((process.env.DATABASE_URL || mongoDB), { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.connect(process.env.DATABASE_URL || mongoDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
 
 // Matches with "/api/mid/.../.../.../...."
 // ________________________________________
@@ -24,16 +28,16 @@ mongoose.connect((process.env.DATABASE_URL || mongoDB), { useNewUrlParser: true,
 // Find a mid-point from 2 sets of latitude and longitude
 // then request a list of buisness names in that lat long
 
-router.get("/mid/:lat1/:long1/:lat2/:long2", function (req, res, next) {
-  function degrees_to_radians(degrees) {
+router.get("/mid/:lat1/:long1/:lat2/:long2", function(req, res) {
+  function degreesToRadians(degrees) {
     var pi = Math.PI;
     return degrees * (pi / 180);
   }
 
-  function radians_to_degrees(radians) {
-    return radians * 180 / Math.PI;
+  function radiansToDegrees(radians) {
+    return (radians * 180) / Math.PI;
   }
- 
+
   // https://stackoverflow.com/questions/4656802/midpoint-between-two-latitude-and-longitude#4656937
 
   let nlat1 = req.params.lat1;
@@ -42,43 +46,46 @@ router.get("/mid/:lat1/:long1/:lat2/:long2", function (req, res, next) {
   let nlat2 = req.params.lat2;
   let nlon2 = req.params.long2;
 
-  console.log(nlat1,nlon1,nlat2,nlon2)
-  let dLon = degrees_to_radians(nlon2 - nlon1);
+  console.log(nlat1, nlon1, nlat2, nlon2);
+  let dLon = degreesToRadians(nlon2 - nlon1);
 
-  let lat1 = degrees_to_radians(nlat1);
-  let lat2 = degrees_to_radians(nlat2);
-  let lon1 = degrees_to_radians(nlon1);
+  let lat1 = degreesToRadians(nlat1);
+  let lat2 = degreesToRadians(nlat2);
+  let lon1 = degreesToRadians(nlon1);
 
-  let Bx = Math.cos(lat2) * Math.cos(dLon)
-  let By = Math.cos(lat2) * Math.sin(dLon)
+  let Bx = Math.cos(lat2) * Math.cos(dLon);
+  let By = Math.cos(lat2) * Math.sin(dLon);
 
-  let lat3 = Math.atan2(Math.sin(lat1) + Math.sin(lat2), Math.sqrt((Math.cos(lat1) + Bx) * (Math.cos(lat1) + Bx) + By * By));
-  let lon3  = lon1 + Math.atan2(By, Math.cos(lat1) + Bx); 
+  let lat3 = Math.atan2(
+    Math.sin(lat1) + Math.sin(lat2),
+    Math.sqrt((Math.cos(lat1) + Bx) * (Math.cos(lat1) + Bx) + By * By)
+  );
+  let lon3 = lon1 + Math.atan2(By, Math.cos(lat1) + Bx);
 
-  let lat = radians_to_degrees(lat3)
-  let lon = radians_to_degrees(lon3)
+  let lat = radiansToDegrees(lat3);
+  let lon = radiansToDegrees(lon3);
 
   req.url = `/mp/${lat}/${lon}`;
   router.handle(req, res);
-  
 });
 
-router.get("/mp/:lat1/:long1", function (req, res, next) {
+router.get("/mp/:lat1/:long1", function(req, res) {
   let nlat1 = req.params.lat1;
   let nlon1 = req.params.long1;
 
-  client.search({
-    latitude: nlat1,
-    longitude: nlon1,
-    radius: 33
-  }).then(response => {
-    res.json(response.jsonBody);
-    console.log(response.jsonBody);
-  }).catch(e => {
-    console.log(e);
-  });
-}
-)
+  client
+    .search({
+      latitude: nlat1,
+      longitude: nlon1,
+      radius: 33
+    })
+    .then(response => {
+      res.json(response.jsonBody);
+      console.log(response.jsonBody);
+    })
+    .catch(e => {
+      console.log(e);
+    });
+});
 
 module.exports = router;
-
