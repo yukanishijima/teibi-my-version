c4798ccb6cf4910b31b13133dd95a584592e1150
Héctor Morales <hector@morales.is>
2019-10-29 19:07:29 -0400

    update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var express = require('express');
+var express = require("express");
 var session = require("express-session");
 
 var passport = require("./config/passport");
@@ -6,32 +6,31 @@ var passport = require("./config/passport");
 var mongoose = require("mongoose");
 var db = require("./models");
 
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var apiRouter = require('./routes/api');
-var ipRouter = require('./routes/ip');
-
+var apiRouter = require("./routes/api");
+// var ipRouter = require('./routes/ip');
 
 var app = express();
 
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/teibidb");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(express.static("client/build"));
 
-app.use('/api', apiRouter);
-app.use('/api', ipRouter);
-
+app.use("/api", apiRouter);
+// app.use('/api', ipRouter);
 
 module.exports = app;
-
