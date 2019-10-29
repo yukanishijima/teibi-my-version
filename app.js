b8e8a76b29ba2e9aa69606bb4ece861a4832b370
Héctor Morales <hector@morales.is>
2019-10-29 10:53:58 -0400

    reset commented sections

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var session = require("express-session");
 
-// var passport = require("./config/passport"); //here
+var passport = require("./config/passport"); //here
 
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -17,8 +17,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.static("client/build"));
 
