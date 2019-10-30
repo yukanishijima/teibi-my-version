31939da6216e3c3c664913321783e1a5fea4eae5
cjstanzhu <cjstanzhu@gmail.com>
2019-10-30 17:45:17 -0400

    setup signup page route and component

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './pages/landing';
 import Main from './pages/main';
 import Signin from './pages/signin';
-import Signup from './pages/signup';
+import SignUp from './pages/signup';
 import NoMatch from './pages/nomatch';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
             <Route exact path="/" component={Landing} />
             <Route path="/main/:id" component={Main} />
             <Route exact path="/signin" component={Signin} />
-            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/signup" component={SignUp} />
             <Route path="*" component={NoMatch} />
           </Switch>
         </Router>
--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import API from "../utils/API";
 
 class SignUp extends Component {
@@ -6,6 +6,7 @@ class SignUp extends Component {
         username: "",
         email: "",
         password: ""
+
     };
 
     handleInputChange = event => {
@@ -22,14 +23,12 @@ class SignUp extends Component {
             password: this.state.password
         };
 
-        API.saveUser({userData})
+        API.saveUser(userData)
             .then(res => {
                 console.log("user saved");
-
             })
             .catch(err => {
                 console.log(err)
-
             });
 
     };
@@ -37,7 +36,53 @@ class SignUp extends Component {
     render() {
         return (
             <>
-                <h1>Sign up</h1>
+                <p>{this.state.username}</p>
+                <p>{this.state.email}</p>
+                <p>{this.state.password}</p>
+
+                <div className="row">
+                    <div className="col-12">
+                        <div className="card">
+                            <h4 className="card-header">Sign Up</h4>
+
+                            <div className="card-body">
+                                <form>
+                                    <div className="form-group">
+                                        <label htmlFor="username-input">Username:</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            id="username-input"
+                                            name="username"
+                                            // value=""
+                                            onChange={this.handleInputChange}
+                                        />
+                                        <label htmlFor="email-input">Email:</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            id="email-input"
+                                            name="email"
+                                            // value=""
+                                            onChange={this.handleInputChange}
+                                        />
+                                        <label htmlFor="password-input">Password:</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            id="password-input"
+                                            name="password"
+                                            // value=""
+                                            onChange={this.handleInputChange}
+                                        />
+                                    </div>
+                                    <button type="submit" className="btn btn-primary" id="sign-up" onClick={this.handleFormSubmit}>Submit</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </>
         )
     }
