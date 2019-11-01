90b0ecc58c6263e4b8b00f392f0f99e7ae1c879e
Héctor Morales <hector@morales.is>
2019-10-31 20:56:19 -0400

    clean up app

--- app.js
+++ app.js
@@ -5,19 +5,12 @@ var session = require("express-session");
 
 var passport = require("./config/passport");
 
-var mongoose = require("mongoose");
-var db = require("./models");
-
-var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var apiRouter = require("./routes/api");
-// var ipRouter = require('./routes/ip');
 
 var app = express();
-
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/teibidb");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -33,6 +26,5 @@ app.use(passport.session());
 app.use(express.static("client/build"));
 
 app.use("/api", apiRouter);
-// app.use('/api', ipRouter);
 
 module.exports = app;
