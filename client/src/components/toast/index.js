d5bc8550716ae23cd6d5a7fa278d71eb56d4919f
Yuka Nishijima <48897211+yukanishijima@users.noreply.github.com>
2019-10-31 21:00:12 -0400

    Update socket room

--- client/src/components/toast/index.js
+++ client/src/components/toast/index.js
@@ -2,19 +2,17 @@ import React, { Component } from 'react';
 import io from "socket.io-client";
 
 // initialize socket
-const socket = io("/");
+const socket = io();
 
 // catch connection test event from server and display on page
 socket.on("connection test", msg => {
   console.log(msg);
 });
 
-// catch status event from server and dispay to page
+// catch status event from server and display to page
 socket.on("joinRoom", msg => {
   console.log(msg);
 });
-
-
 
 
 
@@ -25,21 +23,28 @@ class Toast extends Component {
   }
 
   componentDidMount() {
-    // check if user is online
+    // listen when there's a change in online/offline status
     window.addEventListener('online', this.updateOnlineStatus());
   }
 
   // check online or offline
   updateOnlineStatus() {
+    // check if user is online
     let status;
     (navigator.onLine) ? status = "online" : status = "offline";
 
+    // get the last 9 digits from url (XXXX-XXXX)
     let room = window.location.href;
-    room = status + room.substring(room.lastIndexOf("/") + 1);
+    room = room.substring(room.lastIndexOf("/") + 1);
 
-    // emit status event to server
-    // socket.emit("status", status);
-    socket.emit("joinRoom", room);
+    // create object to store user information
+    const userInfo = {
+      status: status,
+      room: room
+    }
+
+    // emit joinRoom event to server 
+    socket.emit("joinRoom", userInfo);
   }
 
 
