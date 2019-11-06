efdff4391bf5edb4fcea5a5572aacf7dc6c1bd66
cjstanzhu <cjstanzhu@gmail.com>
2019-11-06 11:46:39 -0500

    comment out testing code

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
-import API from "../utils/API";
+// import API from "../utils/API";
 import Help from './help';
 import RandomUrl from '../components/randomurl';
 
 // function Landing() {
 class Landing extends Component {
-  state = {
-    user: "guest1",
+  // state = {
+  //   user: "guest1",
     
-  };
+  // };
 
-  componentDidMount() {
-    API.checkLogin().then(res => {
-      console.log(res.data);
-      this.setState({user: res.data.username});
-      }
-    );
+  // componentDidMount() {
+  //   API.checkLogin().then(res => {
+  //     console.log(res.data);
+  //     console.log(res.data.username);
+  //     this.setState({user: res.data.username});
+  //     }
+  //   );
 
-  };
+  // };
 
   render() {
     return (
       <>
-        <p>{this.state.user}</p>
+        {/* <p>{this.state.user}</p> */}
         <div className="App">
           <Help />
           <RandomUrl />
