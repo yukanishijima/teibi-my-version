25ee8211909b372cb8b570f2ee772fd8da68af12
Héctor Morales <hector@morales.is>
2019-11-07 18:07:00 -0500

    add id to location list

--- client/src/components/list/index.js
+++ client/src/components/list/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 function ListLocations({data}) {
-    return data.length > 0 ? (
-        <ul>
-            {data.map(e => {
+    return (
+        <ul id="locationList">
+            {data.businesses.map(e => {
                 return (
                     <li key={e.name}>
                     <img src={e.image_url} width="50" alt={e.name}></img>{e.name}
