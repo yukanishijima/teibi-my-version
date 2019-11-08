fa23d26642d3eef60ca438e36c95cf9d6ec88e61
Héctor Morales <hector@morales.is>
2019-11-07 19:58:14 -0500

    call data

--- client/src/components/list/index.js
+++ client/src/components/list/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ListLocations({data}) {
     return data.length > 0 ? (
         <ul id="locationList">
-            {data.businesses.map(e => {
+            {data.map(e => {
                 return (
                     <li key={e.name}>
                     <img src={e.image_url} width="50" alt={e.name}></img>{e.name}
