2439a89f5283b697719bae2025319052326e48ec
Yuka Nishijima <48897211+yukanishijima@users.noreply.github.com>
2019-11-07 20:40:49 -0500

    Add dialog component

--- client/src/components/infobutton/index.js
+++ client/src/components/infobutton/index.js
@@ -1,11 +1,53 @@
 import React from 'react';
+import SignIn from "../sign/in"
 
-function InfoButton() {
-    return (
-        <>
-            <h2>Info Button</h2>
-        </>
-    )
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+// import DialogTitle from '@material-ui/core/DialogTitle';
+
+
+export default function InfoButton() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
+  return (
+    <div>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        Menu
+        </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        {/* <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle> */}
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+
+            <DialogActions>
+              <SignIn url={window.location.pathname} />
+
+              <Button onClick={handleClose} color="primary" autoFocus>
+                Agree
+              </Button>
+            </DialogActions>
+
+          </DialogContentText>
+        </DialogContent>
+
+      </Dialog>
+    </div>
+  );
 }
-
-export default InfoButton;
--- client/src/components/sign/in.js
+++ client/src/components/sign/in.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import Button from '@material-ui/core/Button';
 
-function SignIn({url}) {
-    let urlClean = url.split('/')[2]
-    let finalUrl = "/signin/?" + urlClean;
-    return (
-        <Link to={finalUrl}>
-            <button>Sign in</button>
-          </Link>
-    )
+
+function SignIn({ url }) {
+  let urlClean = url.split('/')[2]
+  let finalUrl = "/signin/?" + urlClean;
+
+  const [setOpen] = React.useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
+  return (
+    <Link to={finalUrl}>
+      <Button onClick={handleClose} color="primary" autoFocus>
+        Sign in
+      </Button>
+    </Link>
+  )
 }
 
 export default SignIn;
