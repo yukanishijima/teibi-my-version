2cde12ce31bea4cb46a11a27401bca28f51fd8e6
Héctor Morales <hector@morales.is>
2019-11-07 20:33:33 -0500

    sign in/up paths

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './pages/landing';
 import Main from './pages/main';
+import SignUp from './pages/signup';
+import SignIn from './pages/signin';
 import NoMatch from './pages/nomatch';
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
           <Switch>
             <Route exact path="/" component={Landing} />
             <Route path="/main/:id" component={Main} />
+            <Route exact path="/signin" component={SignIn} />
+            <Route exact path="/signup" component={SignUp} />
             <Route path="*" component={NoMatch} />
           </Switch>
         </Router>
