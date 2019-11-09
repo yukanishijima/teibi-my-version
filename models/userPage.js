0f6ef0f06df2327fae05b77c2bbbda5537443c12
Héctor Morales <hector@morales.is>
2019-11-09 17:39:48 -0500

    clean up format

--- models/userPage.js
+++ models/userPage.js
@@ -1,16 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const userPageSchema = new Schema({
-  username:
-  {
+  username: {
     // Store ObjectIds in the array
     type: Schema.Types.ObjectId,
     // The ObjectIds will refer to the ids in the User model
     ref: "User"
-  }
-,
-  userImage: {type: String},  
-  savedPlaces: {type: String}
+  },
+  userImage: { type: String },
+  savedPlaces: { type: String }
 });
 const UserPage = mongoose.model("UserPage", userPageSchema);
 module.exports = UserPage;
