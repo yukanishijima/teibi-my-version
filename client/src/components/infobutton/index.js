14d7bb07216b5b6a7ece75ec66b64157a7bf2155
Yuka Nishijima <48897211+yukanishijima@users.noreply.github.com>
2019-11-09 10:37:21 -0500

    Add styling

--- client/src/components/infobutton/index.js
+++ client/src/components/infobutton/index.js
@@ -4,9 +4,13 @@ import React from 'react';
 
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
+// import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+import './style.css';
+
 
 export default function InfoButton() {
   const [open, setOpen] = React.useState(false);
@@ -18,6 +22,10 @@ export default function InfoButton() {
   const handleClose = () => {
     setOpen(false);
   };
+
+  const handleDisplay = () => {
+    // add code here
+  }
 
   let urlClean = window.location.pathname.split('/')[2]
   let finalUrlSignIn = "/signin/?" + urlClean;
@@ -35,17 +43,23 @@ export default function InfoButton() {
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
+        <DialogTitle id="alert-dialog-title">
+          <Button onClick={handleDisplay}>Sign In</Button>
+          <Button onClick={handleDisplay}>Sign Up</Button>
+        </DialogTitle>
+
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
 
-            <DialogActions>
-              <iframe id="signIn" title="Sign In" frameBorder="0" src={finalUrlSignIn}>
-              </iframe>
-              </DialogActions>
-              <DialogActions>
-              <iframe id="signOut" title="Sign Out"  frameBorder="0" src={finalUrlSignUp}>
-              </iframe>
-            </DialogActions>
+            {/* <DialogActions> */}
+
+            <iframe id="signIn" className="signIn" title="Sign In" frameBorder="0" src={finalUrlSignIn}>
+            </iframe>
+            {/* </DialogActions> */}
+            {/* <DialogActions> */}
+            <iframe id="signUp" className="signUp hide" title="Sign Up" frameBorder="0" src={finalUrlSignUp}>
+            </iframe>
+            {/* </DialogActions> */}
 
           </DialogContentText>
         </DialogContent>
