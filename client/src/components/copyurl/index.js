30bd1cffe5b8e65dea2ddf8943001103f8d35bdd
Yuka Nishijima <48897211+yukanishijima@users.noreply.github.com>
2019-11-09 14:20:02 -0500

    Add basic styling

--- client/src/components/copyurl/index.js
+++ client/src/components/copyurl/index.js
@@ -1,32 +1,49 @@
 import React from 'react';
 import { useCopyToClipboard } from "react-use";
 import { ToastContainer, toast } from "react-toastify";
-import Button from '@material-ui/core/Button';
 import "react-toastify/dist/ReactToastify.css";
+import Button from '@material-ui/core/Button';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import './style.css';
-//import {  positions } from '@material-ui/system';
+
+// set up default primary color
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#005f56",
+      secondary: "#00897b",
+    },
+    secondary: {
+      main: '#ffe57f',
+    },
+  },
+});
 
 
 const CopyLink = (props) => {
-    const [ setText] = React.useState('');
-    // eslint-disable-next-line
-    const [state, copyToClipboard] = useCopyToClipboard();
-    // console.log(props);
-    
-    function notify (){ 
-      toast.warn("Your Link is copied. Share it!",{
-        position: "bottom-center",
-        autoClose: 2000,
-        pauseOnFocusLoss: false
-      },
-      )
-    };
-    return (
-      <div>
-        <input type="hidden" value={props.text} onChange={e => setText(e.target.value)} />
-        <ToastContainer />
-        <Button id="copyButton" variant="contained" color="primary" type="button" onClick={() => {copyToClipboard(props.text); notify()}}><i className="fas fa-copy"> share Link</i></Button>
-      </div>
+  const [setText] = React.useState('');
+  // eslint-disable-next-line
+  const [state, copyToClipboard] = useCopyToClipboard();
+  // console.log(props);
+
+  function notify() {
+    toast.warn("Your Link is copied. Share it!", {
+      position: "bottom-center",
+      autoClose: 2000,
+      pauseOnFocusLoss: false
+    },
     )
-  }
-  export default CopyLink;
+  };
+  return (
+    <div>
+      <input type="hidden" value={props.text} onChange={e => setText(e.target.value)} />
+      <ToastContainer />
+      <ThemeProvider theme={theme}>
+        <Button id="copyButton" variant="contained" color="primary" type="button" onClick={() => { copyToClipboard(props.text); notify() }}>
+          <i className="fas fa-copy"></i>
+        </Button>
+      </ThemeProvider>
+    </div>
+  )
+}
+export default CopyLink;
--- client/src/components/infobutton/index.js
+++ client/src/components/infobutton/index.js
@@ -1,16 +1,61 @@
 import React from 'react';
 import API from "../../utils/API";
-// import SignIn from "../sign/in";
-// import SignUp from "../sign/up";
-
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
+// import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-
+import { makeStyles, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import './style.css';
+
+// set up default primary color
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#005f56",
+      secondary: "#00897b",
+    },
+    secondary: {
+      main: '#ffe57f',
+    },
+  },
+});
+
+const useStyles = makeStyles({
+  container: {
+    height: "400px",
+    background: "white",
+  },
+  menu: {
+    borderRadius: "50%",
+    position: "absolute",
+    bottom: 10,
+    right: 10,
+    width: 45,
+    minWidth: 45,
+    height: 45,
+  },
+  logoutContainer: {
+    justifyContent: "center",
+  },
+  logout: {
+    border: "1px solid rgb(18, 56, 6)",
+    borderRadius: 50,
+    width: "90%",
+    background: "white",
+    color: "rgb(18, 56, 6)",
+  },
+  titleContainer: {
+    textAlign: "center",
+  },
+  signIn: {
+    color: "rgb(18, 56, 6)",
+  },
+  signUp: {
+    color: "rgb(18, 56, 6)",
+  }
+});
 
 
 export default function InfoButton() {
@@ -26,7 +71,8 @@ export default function InfoButton() {
 
   const handleDisplay = () => {
     // add code here
-  }
+  };
+
   const handleLogOut = () => {
     API.checkLogin().then(res => {
       console.log(res.data.loggedIn);
@@ -39,10 +85,10 @@ export default function InfoButton() {
       } else {
         alert("You're not logged in.");
       };
-
     });
-
   };
+
+  const classes = useStyles();
 
   let urlClean = window.location.pathname.split('/')[2]
   let finalUrlSignIn = "/signin/?" + urlClean;
@@ -50,41 +96,47 @@ export default function InfoButton() {
 
 
   return (
-    <div>
-      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        Menu
+    <>
+      <ThemeProvider theme={theme}>
+        <Button variant="contained" color="primary" type="button" onClick={handleClickOpen} className={classes.menu}>
+          <i class="fas fa-hand-point-right" style={{ fontSize: "1.8em" }}></i>
         </Button>
+      </ThemeProvider>
+
       <Dialog
         open={open}
         onClose={handleClose}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-title">
-          <Button onClick={handleDisplay}>Sign In</Button>
-          <Button onClick={handleDisplay}>Sign Up</Button>
-        </DialogTitle>
+        <DialogContent className={classes.container}>
 
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
+          <DialogTitle id="alert-dialog-title" className={classes.titleContainer}>
+            <Button onClick={handleDisplay} className={classes.signIn}>Sign In</Button>
+            <Button onClick={handleDisplay} className={classes.signUp}>Sign Up</Button>
+          </DialogTitle>
 
-            {/* <DialogActions> */}
+          {/* <DialogContentText id="alert-dialog-description"> */}
+          {/* <DialogActions> */}
 
-            <iframe id="signIn" className="signIn" title="Sign In" frameBorder="0" src={finalUrlSignIn}>
-            </iframe>
-            {/* </DialogActions> */}
-            {/* <DialogActions> */}
-            <iframe id="signUp" className="signUp hide" title="Sign Up" frameBorder="0" src={finalUrlSignUp}>
-            </iframe>
-            {/* </DialogActions> */}
-            <DialogActions>
-              <Button variant="outlined" color="primary" onClick={handleLogOut}>Log Out</Button>
-            </DialogActions>
+          <iframe id="signIn" className="signIn" title="Sign In" frameBorder="0" src={finalUrlSignIn}>
+          </iframe>
 
-          </DialogContentText>
+          {/* </DialogActions> */}
+          {/* <DialogActions> */}
+
+          <iframe id="signUp" className="signUp hide" title="Sign Up" frameBorder="0" src={finalUrlSignUp}>
+          </iframe>
+
+          {/* </DialogActions> */}
+
+          <DialogActions className={classes.logoutContainer}>
+            <Button variant="outlined" color="primary" onClick={handleLogOut} className={classes.logout}>Log Out</Button>
+          </DialogActions>
+
+          {/* </DialogContentText> */}
         </DialogContent>
-
       </Dialog>
-    </div >
+    </>
   );
 }
