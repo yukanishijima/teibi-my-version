e8fcd094631e2b12bceb6acd14ecd95901fb5ae4
Héctor Morales <hector@morales.is>
2019-11-14 13:25:59 -0500

    basic help

--- client/src/pages/help.js
+++ client/src/pages/help.js
@@ -1,15 +1,47 @@
-import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import "./style.css";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Tooltip from "@material-ui/core/Tooltip";
+import Fab from '@material-ui/core/Fab';
+import Dialog from '@material-ui/core/Dialog';
+import TogglePanel from "../components/help";
+// import { myTheme } from "../utils/myTheme";
+import '../components/infobutton/style.css';
 
-class Help extends Component {
-  render() {
-    return (
-      <>
-        <Button><img id="helpIcon" src="/images/LogoPadded.png" alt="logo"/></Button>
-      </>
-    )
-  }
+
+const useStyles = makeStyles(theme => ({
+  fab: {
+    margin: theme.spacing(1),
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
+
+
+export default function Help() {
+
+  const classes = useStyles();
+
+  const [open, setOpen] = React.useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
+  return (
+    <>
+      <Fab color="primary" aria-label="add" className={classes.fab} onClick={handleClickOpen} id="help">
+        <Tooltip title={"User"} placement="top">
+        <img id="helpIcon" src="/images/Logo2.png" alt="logo"/>
+        </Tooltip>
+      </Fab>
+
+      <Dialog open={open} onClose={handleClose} className="dialog">
+        <TogglePanel />
+      </Dialog>
+    </>
+  );
 }
-
-export default Help;
