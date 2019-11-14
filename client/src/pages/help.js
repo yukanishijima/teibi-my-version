d09f5b9639a130c6d4b55e08abbe03dec36021ec
Héctor Morales <hector@morales.is>
2019-11-13 21:44:39 -0500

    REAME WIP and help

--- client/src/pages/help.js
+++ client/src/pages/help.js
@@ -6,6 +6,18 @@ class Help extends Component {
         return (
             <>
                 <h1>Help</h1>
+                <ol>
+                    <li>Copy the URL 🔗</li>
+                    <li>Share link</li>
+                    <li>Select your starting point</li>
+                    <li>Wait for the other user</li>
+                    <li>Once second user selects their location a mid point will be calculated</li>
+                    <li>A list of places will be presented</li>
+                    <li>See you there</li>
+                    <li>See you next time</li>
+                </ol>
+                Email us with any questions or suggestions:
+                <a href="mailto:info@teibi.ca">HELP</a>
             </>
         )
     }
