b4e3c4d353ac9d280ec6522880a2b89ed0ae3331
Héctor Morales <hector@morales.is>
2019-11-29 00:10:30 -0500

    accept yelp errors

--- client/src/components/list/index.js
+++ client/src/components/list/index.js
@@ -22,50 +22,61 @@ const handleHideList = (e) => {
 }
 
 function ListLocations({ data, displayClass }) {
+  if(typeof(data.error) !== "undefined"){
+    // console.log(data.error)
+    return (
 
-  return data.length > 0 ? (
-    <div id="locationList">
-      <List id="list" style={{ backgroundColor: myTheme.palette.secondary.main }}>
+      <div id="locationList" className={displayClass}>
+        <List id="progress" style={{ backgroundColor: myTheme.palette.secondary.main }} key={data.error.code}>
+        <p>
+          {data.error.code}
+        </p>
+        </List>
+        <Button size="medium" id="hide-btn" onClick={handleHideList}><i className="fas fa-angle-double-up"></i></Button>
+      </div>
+    )
+  }else{
+  if(typeof(data.businesses) !== "undefined"){
+    return data.businesses.length > 0 ? (
+      <div id="locationList">
+        <List id="list" style={{ backgroundColor: myTheme.palette.secondary.main }}>
 
-        {data.map((e, k) => {
-          return (
-            <>
-              <ListItem key={e.name} alignItems="center" id="list-item">
+          {data.businesses.map((e, k) => {
+            return (
+                <ListItem key={e.name} alignItems="center" id="list-item">
+                  <ListItemAvatar id="avater">
+                    <Avatar alt={e.name} src={e.image_url} />
+                  </ListItemAvatar>
+                  <ListItemText id="content"
+                    primary={<>
+                      <span id="placeName" style={{ color: myTheme.palette.primary.main }}>{e.name}</span>
 
-                <ListItemAvatar id="avater">
-                  <Avatar alt={e.name} src={e.image_url} />
-                </ListItemAvatar>
+                    </>}
+                    secondary={
+                      <>
+                        <a href={"tel:" + e.phone} id="placePhone" style={{ color: myTheme.palette.primary.grey }}>{e.phone}</a>
+                        <span id="placeAddress">{e.location.address1}</span>
+                      </>
+                    }
+                  />
+                  <i className="fas fa-heart" onClick={handleToggle} id="fav-btn"></i>
+                </ListItem>
+            )
+          })}
+        </List>
+        <Button size="medium" id="hide-btn" onClick={handleHideList}><i className="fas fa-angle-double-up"></i></Button>
+      </div>
+    ) : <div id="locationList" className={displayClass}>
+        <List id="progress" style={{ backgroundColor: myTheme.palette.secondary.main }}>
+          <CircularProgress />
+        </List>
+        <Button size="medium" id="hide-btn" onClick={handleHideList}><i className="fas fa-angle-double-up"></i></Button>
 
-                <ListItemText id="content"
-                  primary={<>
-                    <span id="placeName" style={{ color: myTheme.palette.primary.main }}>{e.name}</span>
-
-                  </>}
-                  secondary={
-                    <>
-                      <a href={"tel:" + e.phone} id="placePhone" style={{ color: myTheme.palette.primary.grey }}>{e.phone}</a>
-                      <span id="placeAddress">{e.location.address1}</span>
-                    </>
-                  }
-                />
-
-                <i className="fas fa-heart" onClick={handleToggle} id="fav-btn"></i>
-
-              </ListItem>
-
-            </>
-          )
-        })}
-      </List>
-      <Button size="medium" id="hide-btn" onClick={handleHideList}><i class="fas fa-angle-double-up"></i></Button>
-    </div>
-  ) : <div id="locationList" className={displayClass}>
-      <List id="progress" style={{ backgroundColor: myTheme.palette.secondary.main }}>
-        <CircularProgress />
-      </List>
-      <Button size="medium" id="hide-btn" onClick={handleHideList}><i className="fas fa-angle-double-up"></i></Button>
-
-    </div>;
+      </div>;
+    }else{
+      return(<></>);
+    }
+  }
 }
 
 export default ListLocations;
