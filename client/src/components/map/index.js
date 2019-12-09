de949a1bc9c71d8c1f82e20e0b71de8ba114446b
Yuka Nishijima <48897211+yukanishijima@users.noreply.github.com>
2019-12-09 18:39:07 -0500

    Add css sizing fix snippet

--- client/src/components/map/index.js
+++ client/src/components/map/index.js
@@ -8,6 +8,19 @@ import './style.css'
 // initialize socket
 import { socket } from "../socket";
 
+// correct sizing for mobile
+// get the viewport height and multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+// listen to the resize event
+window.addEventListener('resize', () => {
+  // execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+
 let map = "";
 let marker = "";
 
